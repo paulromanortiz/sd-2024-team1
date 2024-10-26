46299953ba3d47a10d2ddaf9c623cbdeaf5a839f
pjean <164891302+pjean13@users.noreply.github.com>
2024-10-26 16:58:24 +0000

    #indents

--- budget_tool/budget/views.py
+++ budget_tool/budget/views.py
@@ -5,12 +5,12 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 def home(request):
-return render(request, 'main/home.html')
+    return render(request, 'main/home.html')
 
 @login_required
 def dashboard(request):
-notifications = [
+    notifications = [
 "Welcome to the dashboard!",
 "You have no new messages.",
 ]
-return render(request, 'main/dashboard.html', {'notifications': notifications})
+    return render(request, 'main/dashboard.html', {'notifications': notifications})
