d5eacdb325e47828818681eb239741bfb98bc6ea
Javier Benítez <javier.benitez36@yahoo.com>
2024-10-26 16:52:53 +0000

    #set-up compage and dashboard

--- budget_tool/budget_tool/urls.py
+++ budget_tool/budget_tool/urls.py
@@ -23,3 +23,11 @@ path('admin/', admin.site.urls),
 path('accounts/', include('allauth.urls')), # Allauth authentication
 path('', include('main.urls')), # Main app URLs
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+path('', views.home, name='home'),
+path('dashboard/', views.dashboard, name='dashboard'),
+]
