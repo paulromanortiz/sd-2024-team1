f0941bd8d1d8a359026e441939f3ef201332201a
pjean <164891302+pjean13@users.noreply.github.com>
2024-11-02 16:02:17 +0000

    #fail

--- budget_tool/budget/views.py
+++ budget_tool/budget/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'budget/home.html')
-@csrf_exempt
+
 @login_required
 def dashboard(request):
     notifications = [
